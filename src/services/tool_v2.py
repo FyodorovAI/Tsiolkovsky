f9437b4dbed7d4dfc1c5920d253f8a2d9dd8c3c3
Daniel Ransom <dantheman0207@users.noreply.github.com>
2025-04-17 18:49:27 +0200

    fix tool type when updating in db

--- src/services/tool_v2.py
+++ src/services/tool_v2.py
@@ -21,13 +21,14 @@ class MCPTool():
             raise e
 
     @staticmethod
-    def update_in_db(access_token: str, id: str, tool: dict) -> dict:
+    def update_in_db(access_token: str, id: str, tool: ToolModel) -> dict:
         if not id:
             raise ValueError('Tool ID is required')
         try:
             supabase = get_supabase(access_token)
-            print('updating tool in db', tool)
-            result = supabase.table('mcp_tools').update(tool).eq('id', id).execute()
+            tool_dict = tool.to_dict()
+            print('updating tool in db', tool_dict)
+            result = supabase.table('mcp_tools').update(tool_dict).eq('id', id).execute()
             return result.data[0]
         except Exception as e:
             print('An error occurred while updating tool:', id, str(e))
